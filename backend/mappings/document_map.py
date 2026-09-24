cfd45d4a4504dab87034f50cc219b4833401f344
dochikhoa <dochikhoa2006@gmail.com>
2026-09-24 13:49:00 -0400

    get full context dicument

--- backend/mappings/document_map.py
+++ backend/mappings/document_map.py
@@ -45,6 +45,11 @@ class DocumentMap:
 
         return list(self._documents)
 
+    def get_full_text(self, document_id: str) -> str:
+        key = required_uuid(document_id, "document_id")
+        return "".join(self._documents[key].values())
+
+
     def update_paragraphs(
         self,
         document_id: str,
